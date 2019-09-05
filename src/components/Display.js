496fb1e4c27843dffe22517aecb2c9f9392bbfdb
Brandon-Harris <brandonharris177@gmail.com>
2019-09-05 14:05:51 -0600

    basic layout

---
+++ src/components/Display.js
@@ -0,0 +1,12 @@
+import React, {useState} from "react";
+
+const Display = () => {
+    
+    return (
+        <>
+            <h1>is it working</h1>
+        </>
+    );
+};
+
+export default Display
